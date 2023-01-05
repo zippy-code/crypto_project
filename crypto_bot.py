d71e8c9341872415c5491f3606eea0b013626ffb
zippy-code <seonjoo2030@hanmail.net>
2023-01-05 13:44:46 +0900

    메시지 적당히 출력되도록 수정

--- crypto_bot.py
+++ crypto_bot.py
@@ -2,23 +2,37 @@ from apscheduler.schedulers.background import BlockingScheduler
 import requests
 import crypto_bot_auth as cauth
 
+flag = True
+old_result = 0
+
 def bitAlert():
 
     global scheduler
+    global flag
+    global old_result
 
     result = requests.get("https://api.cryptoquant.com/v1/btc/exchange-flows/inflow?exchange=all_exchange&window=block&limit=5",
         headers = {
             'Authorization': cauth.Authorization_cryptoquant
         }).json()
 
-    if (result['result']['data'][0]["inflow_mean"] > 5):
+    if (result['result']['data'][0]["inflow_mean"] > 5 and flag):
         requests.post("https://slack.com/api/chat.postMessage",
-            headers = { "Authorization": "Bearer xoxb-4595599626609-4580262216405-vMKc57gldJK2GiWmSCplchax" },
+            headers = { "Authorization": "Bearer xoxb-4595599626609-4580262216405-ewI7u3VItTJlrsChs6MEm93l" }, 
             data= {
                 "channel": "#checkbot",
-                "text": str(result['result']['data'][0]["inflow_mean"]) +"개 비트코인 -> 거래소 이동 포착! "
+                "text": str(result['result']['data'][0]["inflow_mean"]) +"개 비트코인 -> 거래소 이동"
                 }
             )
+        flag = False
+
+    elif (old_result != result['result']['data'][0]["inflow_mean"]):
+        #flag off
+        old_result = result['result']['data'][0]["inflow_mean"]
+        flag = True
+    
+
+    print(result['result']['data'][0]["inflow_mean"])
 
 scheduler = BlockingScheduler()
 scheduler.add_job(bitAlert, 'interval', seconds = 5, id = 'example', args = [])
